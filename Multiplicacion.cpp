dd00e6a326ba20ba85330fd6d1f2b4ebda882fee
00225820 <62627345+00225820@users.noreply.github.com>
2020-09-10 17:32:19 -0600

    Create Multiplicacion.cpp

---
+++ Multiplicacion.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int multi(int a, int b) //Función para poder discriminar el valor del número
+{
+  if(b == 1){
+    return a;
+  }
+  return a + multi(a, b-1);
+}
+
+
+int main() //Llamada de funciones
+{
+  int a;
+  int b;
+
+  cout << "Ingrese el valor de a: ";
+  cin >> a;
+
+  cout << "Ingrese el valor de b: ";
+  cin >> b;
+
+  if (a<=0 || b<=0)
+  {
+    cout <<"ERROR"<<endl;
+    exit(0);
+  }
+  
+  cout << "El producto de ambos enteros es: ";
+  cout <<  multi(a,b); 
+}
